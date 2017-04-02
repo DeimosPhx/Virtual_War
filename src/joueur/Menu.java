9680a0c035cf266c996e591961f230923dac563f
CNotrePointG <ntahuy@outlook.fr>
2017-04-02 21:19:53 +0200

    Update Menu.java

--- src/joueur/Menu.java
+++ src/joueur/Menu.java
@@ -1,5 +1,4 @@
 package joueur;
-import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import unite.*;
 import Terrain.*;
@@ -7,30 +6,25 @@ public class Menu {
 	/*
 	 * Objectifs:
 	 * 	acceuillir le joueur
-	 *  on fait composer les équipes des 2 joueurs
+	 *  on fait composer les Ã©quipes des 2 joueurs
 	 *  ??
 	 */
 	
 	/*	showInputDialog(null,String message);
 	 * BESOINS:
 	 * 	compo #arraylist
-	 *  créer base avec compo #return base
+	 *  crÃ©er base avec compo #return base
 	 *  generateur terrain #return terrain
 	 */
 	public static Plateau acceuil(){
 		/* JOptionPane pour dire bonjour */
 		JOptionPane.showMessageDialog(null, "Bienvenue dans Virtual War !");
-		String taux = JOptionPane.showInputDialog(null,"Taux d'obstacles : ");
-		int tx = Integer.valueOf(taux);
+		//String taux = JOptionPane.showInputDialog(null,"Taux d'obstacles : ");
+		//int tx = Integer.valueOf(taux);
 		Plateau plat = new Plateau(10,10);
 		/*
 		 * on genere les obstacles selon le taux
 		 */
-		
-		/*Base b1 = new Base(new Coordonnees(0,0),1);
-		Base b2 = new Base(new Coordonnees(9,9),2);*/
-		/*plat.setBase(b1);
-		plat.setBase(b2);*/
 		return plat;
 	}
 	public boolean obstacleLigCol(Plateau plat){
@@ -61,7 +55,7 @@ public class Menu {
 		Coordonnees actuel = new Coordonnees(0,0);
 		//Pour savoir si la coordonnees actuel ne descend pas en dessous du tableau
 		while(plat.estDans(new Coordonnees(actuel.getAbscisse()+1,actuel.getOrdonnee()))){
-			//Pour savoir si la case en diagonale à droite sera encore dans le tableau
+			//Pour savoir si la case en diagonale Ã  droite sera encore dans le tableau
 			while(plat.estDans(new Coordonnees((actuel.getAbscisse()-1),actuel.getOrdonnee()+1))){
 				actuel = new Coordonnees(actuel.getAbscisse()-1,actuel.getOrdonnee()+1);
 				int cpt=0;
@@ -81,40 +75,45 @@ public class Menu {
 	public boolean obstaclebase(Plateau plat, Coordonnees coord){
 		return (coord.getAbscisse()>4 && coord.getOrdonnee()>4 && (coord.getAbscisse()<(plat.getX()-3))&& (coord.getOrdonnee()<(plat.getY()-3)));  
 	}
-	public static ArrayList<Robot> compo(Joueur j,Plateau plat){
-		//int nbRobot = 0;
-		//String nbRobot1 = JOptionPane.showInputDialog(null,"J"+j.getEquipe() + "\n entrez le nombre de robot souhaité : ");
-		//nbRobot = Integer.valueOf(nbRobot1);
-		JOptionPane.showMessageDialog(null, "J"+j.getEquipe()+": formez votre armée !");
-		ArrayList<Robot> armee = new ArrayList<Robot>();
-		for(int i=0; i<3; i++){
+	public static void compo(Joueur j,Plateau plat){
+		int nbRobot = 0;
+        while (true) {
+            String nbRobot1 = JOptionPane.showInputDialog(null,"J"+j.getEquipe() + "\n entrez le nombre de robot souhaitÃ©, entre 1 et 9 : ");
+            if ( nbRobot1.charAt(0) > '0' && nbRobot1.charAt(0) <= '9') {
+                nbRobot = Integer.valueOf(nbRobot1);
+                break;
+            }
+        }
+		for(int i=0; i<nbRobot; i++){
 			boolean isOut;
 			do{
 				isOut = true;
-				String selec = JOptionPane.showInputDialog(null,"Quel robot souhaitez vous utiliser ?\n CHAR \n PIEGEUR \n TIREUR");
+				String selec = JOptionPane.showInputDialog(null,"Quel robot souhaitez vous utiliser ?\n char \n piegeur \n tireur");
 			
 			switch(selec){
-			case "CHAR": 
-				Char c1 = new Char(j.getEquipe(),new Coordonnees((j.getEquipe()-1)*10, (j.getEquipe()-1)*10), plat);
-				armee.add(c1);
+			case "char": 
+				Char c1 = new Char(j.getEquipe(),new Coordonnees((j.getEquipe()-1)*9, (j.getEquipe()-1)*9), plat);
+				j.addRobot(c1);
+				isOut = false;
 				/*
-				 * creer une unitée et l'ajouter à l'arraylist de la base du joueur (selon son equipe)
+				 * ecreer une unitÃ©e et l'ajouter Ã  l'arraylist de la base du joueur (selon son equipe)
 				 */
 				break;
-			case "PIEGEUR":
-				Piegeur p1 = new Piegeur(j.getEquipe(),new Coordonnees((j.getEquipe()-1)*10, (j.getEquipe()-1)*10), plat);
-				armee.add(p1);
+			case "piegeur":
+				Piegeur p1 = new Piegeur(j.getEquipe(),new Coordonnees((j.getEquipe()-1)*9, (j.getEquipe()-1)*9), plat);
+				j.addRobot(p1);
+				isOut = false;
 				break;
-			case "TIREUR":
-				Tireur t1 = new Tireur(j.getEquipe(),new Coordonnees((j.getEquipe()-1)*10, (j.getEquipe()-1)*10), plat);
-				armee.add(t1);
+			case "tireur":
+				Tireur t1 = new Tireur(j.getEquipe(),new Coordonnees((j.getEquipe()-1)*9, (j.getEquipe()-1)*9), plat);
+				j.addRobot(t1);
+				isOut = false;
 				break;
 				default:
 					JOptionPane.showMessageDialog(null, "ERREUR ENTREE INVALIDE");
-					isOut = false;
 			}
-			}while(!isOut);
+			}while(isOut);
 		}
-		return armee;
+		j.updateArrays();
 	}
 }
