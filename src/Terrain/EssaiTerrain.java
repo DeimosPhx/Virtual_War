0c66d2b9f5617ff74b316144c33c845c6aadf27b
CNotrePointG <ntahuy@outlook.fr>
2017-04-02 13:53:34 +0200

    Update EssaiTerrain.java

--- src/Terrain/EssaiTerrain.java
+++ src/Terrain/EssaiTerrain.java
@@ -4,6 +4,7 @@ package Terrain;
 
 import joueur.Menu;
 import joueur.Vue;
+import unite.Mine;
 import unite.Piegeur;
 import unite.Tireur;
 
@@ -16,23 +17,34 @@ public class EssaiTerrain {
 		terrain.setBase(new Base(new Coordonnees(0,0),1));
 		terrain.setBase(new Base(new Coordonnees(9,9),1));
 		terrain.setObstacle(new Coordonnees(5,5));
-		terrain.setRobot(new Tireur(1, new Coordonnees(5,6), terrain));
+		Tireur tireur12 = new Tireur(1, new Coordonnees(5,6), terrain);
+		terrain.setRobot(tireur12);
 		Piegeur piegeur1 = new Piegeur(1, new Coordonnees(1, 1), terrain);
 		terrain.setRobot(piegeur1);
 		piegeur1.tirer(Direction.DROITE);
 		Piegeur piegeur2 = new Piegeur(2, new Coordonnees(2, 2), terrain);
-		terrain.setRobot(piegeur1);
+		terrain.setRobot(piegeur2);
 		piegeur2.tirer(Direction.BAS_GAUCHE);
 		Tireur tireur1 = new Tireur(1, new Coordonnees(5, 5), terrain);
 		Tireur tireur2 = new Tireur(2, new Coordonnees(5, 7), terrain);
 		terrain.setRobot(tireur1);
 		terrain.setRobot(tireur2);
+		terrain.setMine(new Coordonnees(6, 5), new Mine(2, new Coordonnees(5, 6)));
+		System.out.println(tireur1.getEnergie());
 		if(tireur1.peutTirer(Direction.DROITE)){
-			System.out.println("Bon");
+			System.out.println("Pas Bon");
 		}
 		if(tireur1.peutTirer(Direction.GAUCHE)){
 			System.out.println("PAS BON !");
 		}
+		if(tireur2.peutTirer(Direction.GAUCHE)){
+			System.out.println("tir!");
+		}
+		if(tireur12.peutTirer(Direction.DROITE)){
+			System.out.println("tir!");
+		}
+		terrain.deplacer(tireur1, Direction.BAS);
+		System.out.println(tireur1.getEnergie());
 		System.out.println(terrain);
 		System.out.println(vue);
 		System.out.println(vue2);
