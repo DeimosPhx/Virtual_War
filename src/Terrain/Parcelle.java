ee40ee0b78ca8e27e5dd9121ae7e394b9577f8fc
TheGoldenTank <popocois@gmail.com>
2017-04-28 17:01:22 +0200

    Update Parcelle.java

--- src/Terrain/Parcelle.java
+++ src/Terrain/Parcelle.java
@@ -49,4 +49,29 @@ public class Parcelle {
 		}
 		return st;
 	}
+	
+	public boolean autoriserPlacementObstacle(Plateau plat,Coordonnees c){
+		if(c.getAbscisse()!=0 && c.getOrdonnee()!=0){
+			if(!plat.getGrille()[c.getAbscisse()+Direction.HAUT_GAUCHE.getAbscisse()][c.getOrdonnee()+Direction.HAUT_GAUCHE.getOrdonnee()].estVide()){
+				return false;
+			}
+		}
+		if(c.getAbscisse()!=0 && c.getOrdonnee()!=9){
+			if(!plat.getGrille()[c.getAbscisse()+Direction.HAUT_DROITE.getAbscisse()][c.getOrdonnee()+Direction.HAUT_DROITE.getOrdonnee()].estVide()){
+				return false;
+			}
+		}
+		if(c.getAbscisse()!=9 && c.getOrdonnee()!=0){
+			if(!plat.getGrille()[c.getAbscisse()+Direction.BAS_GAUCHE.getAbscisse()][c.getOrdonnee()+Direction.BAS_GAUCHE.getOrdonnee()].estVide()){
+				return false;
+			}
+		}
+		if(c.getAbscisse()!=9 && c.getOrdonnee()!=9){
+			if(!plat.getGrille()[c.getAbscisse()+Direction.BAS_DROITE.getAbscisse()][c.getOrdonnee()+Direction.BAS_DROITE.getOrdonnee()].estVide()){
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
