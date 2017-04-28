ff2e3dbe4ae3a34bf283d1efc471b244453c4183
TheGoldenTank <popocois@gmail.com>
2017-04-28 16:47:05 +0200

    Update Obstacle.java

--- src/Terrain/Obstacle.java
+++ src/Terrain/Obstacle.java
@@ -7,4 +7,19 @@ public class Obstacle extends Parcelle{
 	public String toString(){
 		return "O";
 	}
+	public boolean autoriserPlacementObstacle(Plateau plat,Coordonnees c){
+		if(!plat.getGrille()[c.getAbscisse()+Direction.HAUT_GAUCHE.getAbscisse()][c.getOrdonnee()+Direction.HAUT_GAUCHE.getOrdonnee()].estVide()){
+			return false;
+		}
+		if(!plat.getGrille()[c.getAbscisse()+Direction.HAUT_DROITE.getAbscisse()][c.getOrdonnee()+Direction.HAUT_DROITE.getOrdonnee()].estVide()){
+			return false;
+		}
+		if(!plat.getGrille()[c.getAbscisse()+Direction.BAS_GAUCHE.getAbscisse()][c.getOrdonnee()+Direction.BAS_GAUCHE.getOrdonnee()].estVide()){
+			return false;
+		}
+		if(!plat.getGrille()[c.getAbscisse()+Direction.BAS_DROITE.getAbscisse()][c.getOrdonnee()+Direction.BAS_DROITE.getOrdonnee()].estVide()){
+			return false;
+		}
+		return true;
+	}
 }
