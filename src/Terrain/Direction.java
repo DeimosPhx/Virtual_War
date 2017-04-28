26f737dbb49dde8044bdb2a9ec209b140af7fc52
CNotrePointG <ntahuy@outlook.fr>
2017-04-28 15:51:41 +0200

    Update Direction.java

--- src/Terrain/Direction.java
+++ src/Terrain/Direction.java
@@ -1,14 +1,14 @@
 package Terrain;
 
 	public enum Direction {
-		HAUT(new Coordonnees(-1,0)),
-		BAS(new Coordonnees(1,0)),
-		DROITE(new Coordonnees(0,1)),
-		GAUCHE(new Coordonnees(0,-1)),
-		HAUT_GAUCHE(new Coordonnees(-1,-1)),
-		HAUT_DROITE(new Coordonnees(-1,1)),
-		BAS_GAUCHE(new Coordonnees(1,-1)),
-		BAS_DROITE(new Coordonnees(1,1));
+		HAUT		(new Coordonnees(0	,-1)),
+		BAS		(new Coordonnees(0	,1)),
+		DROITE		(new Coordonnees(1	,0)),
+		GAUCHE		(new Coordonnees(-1	,0)),
+		HAUT_GAUCHE	(new Coordonnees(-1	,-1)),
+		HAUT_DROITE	(new Coordonnees(1	,-1)),
+		BAS_GAUCHE	(new Coordonnees(-1	,1)),
+		BAS_DROITE	(new Coordonnees(1	,1));
 		
 		private Coordonnees direction;
 		private Direction(Coordonnees direction) {this.direction=direction;}
